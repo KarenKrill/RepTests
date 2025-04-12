0c848b88ef02aa20c8201a66b0226fa4f7dfceb4
KarenKrill <vampart1997@gmail.com>
2025-04-13 01:04:27 +0500

    Added missing library for github packages

---
+++ SomeClassLibrary/SomeClass.cs
@@ -0,0 +1,15 @@
+﻿namespace SomeClassLibrary
+{
+    public class SomeClass
+    {
+        public int Id { get; set; }
+        public SomeClass(int id)
+        {
+            Id = id;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"SomeObject with id {Id}");
+        }
+    }
+}
