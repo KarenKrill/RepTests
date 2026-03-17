eb2f79149acaa5d9eeb9cad96b145aaf2e090f86
KarenKrill <vampart1997@gmail.com>
2026-03-18 02:36:25 +0500

    Added SomeClass tests

---
+++ SomeClassLibrary.Tests/SomeClassTests.cs
@@ -0,0 +1,13 @@
+﻿namespace SomeClassLibrary.Tests
+{
+    [TestClass]
+    public sealed class SomeClassTests
+    {
+        [TestMethod]
+        public void IdTest()
+        {
+            SomeClass someClass = new(20);
+            Assert.AreEqual(20, someClass.Id);
+        }
+    }
+}
